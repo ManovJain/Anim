6807a20081e20871f892e7f1e06ba460946de096
manovjain <manovj@smu.edu>
2020-11-17 23:54:23 -0600

    Working on sending data between view controllers

--- Anim/View Controllers/SearchViewController.swift
+++ Anim/View Controllers/SearchViewController.swift
@@ -16,9 +16,7 @@ class SearchViewController: UIViewController {
     
     @IBOutlet weak var searchButton: UIButton!
     
-
-    
-    let testURL = "hello.com"
+    public var completionHandler: ((String) -> Void)?
     
     
     override func viewDidLoad() {
@@ -36,6 +34,7 @@ class SearchViewController: UIViewController {
 
     
     @IBAction func searchButton(_ sender: Any) {
+        
 //       var search = searchTextField.text
 //        var url = "http://159.89.231.85:5000/foodSearch/"
 //
@@ -49,6 +48,10 @@ class SearchViewController: UIViewController {
         
         let str = "{ \"image\": [\"img.jpg\"], \"foodName\": [\"Food name\"], \"dietLabels\": [\"Low Carb\"], \"allergyLabels\": [\"Dairy Free\", \"Milk Free\", \"Peanut Free\", \"Tree Nut Free\", \"Soy Free\", \"Fish Free\", \"Shellfish Free\", \"Alcohol Free\"], \"health\": 0, \"Energy\" : [\"1461.59kcal\"], \"Fat\": [\"104.18g\"], \"Carbs\": [\"27.64g\"], \"Sugars\": [\"15.06g\"], \"Protein\": [\"98.16g\"], \"Fiber\": [\"3.59g\"], \"Sodium\": [\"446.2mg\"], \"nutritionalTags\": [\"High Fat\", \"Low Fiber\", \"High Protein\", \"High Cholesterol\"], \"unsustainable\": [\"Palm Oil\"] }"
         let data = Data(str.utf8)
+        
+        completionHandler?(str)
+        
+        dismiss(animated:true, completion: nil)
 
         do {
             // make sure this JSON is in the format we expect
@@ -100,6 +103,7 @@ class SearchViewController: UIViewController {
         } catch let error as NSError {
             print("Failed to load: \(error.localizedDescription)")
         }
+        
         
         
         
